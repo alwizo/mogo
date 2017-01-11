864ccf9f4cdb64d85a31ad19eef0cc263e7c2731
alwizo <alwizo.com@gmail.com>
2017-01-11 17:02:53 +0100

    Added HTML for full page

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
 console.log('This is the Main.js file. It should be the 3rd and final file');
+
+$(document).ready(function() {
+    $( '.portfolio__list' ).masonry( {
+        itemSelector: '.portfolio__item'
+    } );
+});
